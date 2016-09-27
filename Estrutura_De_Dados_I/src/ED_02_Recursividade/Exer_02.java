8df1fced16f3f5895b871c88cbaee0692b6eff27
Wesley A.V <wesley.contato@hotmail.com>
2016-09-26 21:48:51 -0300

    Exer2

---
+++ Estrutura_De_Dados_I/src/ED_02_Recursividade/Exer_02.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ED_02_Recursividade;
+
+/**
+ *
+ * @author Wesley A.V
+ */
+public class Exer_02 {
+    public static void main(String[] args) {
+        double numharm, n;
+        
+        double numharm (in n){
+        if (n == 1)
+        return 1;
+        
+    }
+    }
+}
