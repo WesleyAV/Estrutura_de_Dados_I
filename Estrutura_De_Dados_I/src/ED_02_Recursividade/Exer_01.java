21524512f0c60f5465b626a7aff33217a8e8f094
Wesley A.V <wesley.contato@hotmail.com>
2016-10-11 10:42:29 -0300

    Exer_01

--- Estrutura_De_Dados_I/src/ED_02_Recursividade/Exer_01.java
+++ Estrutura_De_Dados_I/src/ED_02_Recursividade/Exer_01.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ED_02_Recursividade;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Wesley A.V
  */
 public class Exer_01 {
-    
+
+    int tamanho, contador = 1;
+    private String losango = "";
+
+    public void setTamanho() {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite um valor de 1 a 9:");
+        tamanho = ler.nextInt();
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void desenhar(int getTamanho) {
+        if (contador <= getTamanho()) {
+            for (int a = getTamanho(); a > 0; a--) {
+                losango = losango + String.valueOf(contador);
+                System.out.println(losango);
+            }
+            contador = contador + 1;
+            desenhar(getTamanho());
+        }
+    }
+
+    public static void main(String args[]) {
+        Exer_01 obj = new Exer_01();
+        obj.setTamanho();
+        obj.desenhar(obj.getTamanho());
+    }
 }
