781e73d74401d4815e3bcf30a64277365039d8d1
Wesley A.V <wesley.contato@hotmail.com>
2016-10-11 10:42:13 -0300

    Exer_04

--- Estrutura_De_Dados_I/src/ED_02_Recursividade/Exer_04.java
+++ Estrutura_De_Dados_I/src/ED_02_Recursividade/Exer_04.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ED_02_Recursividade;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Wesley A.V
  */
 public class Exer_04 {
-    
+
+    public static void main(String[] args) {
+        Scanner base = new Scanner(System.in);
+        System.out.println("\n Digite o número da Base: ");
+        int base1 = base.nextInt();
+
+        Scanner expo = new Scanner(System.in);
+        System.out.println("\n Digite o núumero do expoente: ");
+        int expoente = expo.nextInt();
+
+        //Imprimindo resultado da Potência
+        System.out.println("O resultado da Potência é de: " + Potencia(base1, expoente));
+
+    }
+
+    public static int Potencia(int y, int k) {
+
+        if (k == 0) {
+            return 1;
+        } else {
+            return Potencia(y, k - 1) * y;
+        }
+    }
 }
