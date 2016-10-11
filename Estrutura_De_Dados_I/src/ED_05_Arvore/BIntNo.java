f50c90dcbd6986d734d819639664a35e5528e58c
Wesley A.V <wesley.contato@hotmail.com>
2016-10-11 10:45:16 -0300

    BIntNo

---
+++ Estrutura_De_Dados_I/src/ED_05_Arvore/BIntNo.java
@@ -0,0 +1,15 @@
+package ED_05_Arvore;
+
+/**
+ *
+ * @author Wesley A.V
+ */
+public class BIntNo {
+
+    int valor;
+    BIntNo esq, dir;
+
+    BIntNo(int novoValor) {
+        valor = novoValor;
+    }
+}
